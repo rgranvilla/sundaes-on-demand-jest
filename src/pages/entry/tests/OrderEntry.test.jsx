1b23c3a1e03e7fd21e20908cb6bd364c6e44c8a8
Ricardo Granvilla <rgranvilla@gmail.com>
2022-03-27 19:55:11 -0300

    fix response return

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -5,12 +5,12 @@ import { server } from '../../../mocks/server';
 
 test('should handlers error for scoops and topping routes', async () => {
   server.resetHandlers(
-    rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-      res(ctx.status(500));
-    }),
-    rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-      res(ctx.status(500));
-    }),
+    rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+      res(ctx.status(500)),
+    ),
+    rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
+      res(ctx.status(500)),
+    ),
   );
 
   render(<OrderEntry />);
