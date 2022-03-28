6cfd649d7f7caf80117e3c711449e6f3df765c38
Ricardo Granvilla <rgranvilla@gmail.com>
2022-03-27 21:45:11 -0300

    Test and code context from scoops subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,6 @@
 import Col from 'react-bootstrap/Col';
 
-export default function ToppingOptions({ name, imagePath }) {
+export default function ToppingOptions({ name, imagePath, updateItemCount }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
